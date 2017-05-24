8216a2556e5faab2f79a596ea4eb629005a2096c
Carlos Villalpando <cv9019@gmail.com>
2017-05-24 10:27:55 -0500

    Initial commit with maven resources

---
+++ src/main/java/com/tecmilenio/RESTfulWS/App.java
@@ -0,0 +1,13 @@
+package com.tecmilenio.RESTfulWS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
